01972d2032beff7653eb7ede2a59f70e5bc5ac41
Armemius <rusgy@yandex.ru>
2025-01-21 22:30:03 +0300

    [+] Add libraries section

---
+++ include/executor/executor.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace minsh::executor {
+
+int execute(const std::vector<std::string> &tokens);
+
+}
---
+++ include/parser/tokenize.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace minsh::parser {
+
+std::vector<std::string> tokenize(const std::string &input);
+
+}
