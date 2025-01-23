11baf0237308199373c595216f1c2e9d1bf7714c
Armemius <rusgy@yandex.ru>
2025-01-23 17:30:30 +0300

    [+] Implement external executor

--- src/controller/executors/external.cpp
+++ src/controller/executors/external.cpp
@@ -5,6 +5,8 @@
 #include <controller/executors/external.hpp>
 #include <cstdlib>
 
+#include "fmt/compile.h"
+
 int minsh::executor::ExternalExecutor::execute(
     const std::vector<std::string> &tokens) const {
   if (tokens.empty()) {
@@ -23,7 +25,8 @@ int minsh::executor::ExternalExecutor::execute(
       perror("Error executing command");
     }
     exit(EXIT_FAILURE);
-  } else if (pid < 0) {
+  }
+  if (pid < 0) {
     perror("Fork failed");
   } else {
     waitpid(pid, nullptr, 0);
@@ -34,5 +37,31 @@ int minsh::executor::ExternalExecutor::execute(
 
 bool minsh::executor::ExternalExecutor::canExecute(
     const std::vector<std::string> &tokens) const {
-  return false;
+  if (tokens.empty()) {
+    return false;
+  }
+
+  if (access(tokens[0].c_str(), X_OK) == 0) {
+    return true;
+  }
+
+  const char *path = getenv("PATH");
+  if (!path) {
+    return false;
+  }
+
+  const std::string &command = tokens[0];
+  std::string pathStr = path;
+  size_t pos = 0;
+  while ((pos = pathStr.find(':')) != std::string::npos) {
+    std::string dir = pathStr.substr(0, pos);
+    std::string fullPath = fmt::format("{}/{}", dir, command);
+    if (access(fullPath.c_str(), X_OK) == 0) {
+      return true;
+    }
+    pathStr.erase(0, pos + 1);
+  }
+
+  const std::string fullPath = fmt::format("{}/{}", pathStr, command);
+  return access(fullPath.c_str(), X_OK) == 0;
 }
