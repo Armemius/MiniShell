fd5479ccc8f8f05f1d0ab45b844f684749bcfdc1
Armemius <rusgy@yandex.ru>
2025-01-23 18:39:59 +0300

    [+] Implement basic internal commands

--- include/controller/executors/internal.hpp
+++ include/controller/executors/internal.hpp
@@ -4,21 +4,26 @@
 #include <controller/executor.hpp>
 #include <functional>
 #include <unordered_map>
+#include <variant>
 
 namespace minsh::executor {
 
 typedef std::shared_ptr<controller::ShellController> controller_ptr;
 
-typedef std::function<int(const std::vector<std::string> &,
-                          const controller_ptr &)>
+typedef std::variant<std::function<int()>,
+                     std::function<int(const std::vector<std::string> &)>,
+                     std::function<int(const std::vector<std::string> &,
+                                       const controller_ptr &)>>
     internal_executor_fn;
 
 template <typename T>
-concept InternalExecutorFn = requires(T t) {
-  {
-    t(std::vector<std::string>{}, controller_ptr{})
-  } -> std::convertible_to<int>;
-};
+concept InternalExecutorFn =
+    (std::is_invocable_v<T> ||
+     std::is_invocable_v<T, const std::vector<std::string> &> ||
+     std::is_invocable_v<T, const std::vector<std::string> &,
+                         const controller_ptr &>)&&requires {
+      { std::declval<T>()() } -> std::convertible_to<int>;
+    };
 
 class InternalExecutor final : public Executor {
   controller_ptr controller;
@@ -34,7 +39,7 @@ class InternalExecutor final : public Executor {
   [[nodiscard]] int execute(
       const std::vector<std::string> &tokens) const override;
 
-  template <InternalExecutorFn T>
+  template <typename T>
   void addCommand(const std::string &name, T &&fn);
 };
 
