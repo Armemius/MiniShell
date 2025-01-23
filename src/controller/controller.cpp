808d639b716b31918566ad3f23b678c70a702d69
Armemius <rusgy@yandex.ru>
2025-01-23 19:10:02 +0300

    [+] Prettify shell output

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -1,16 +1,48 @@
+#include <chrono>
 #include <controller/controller.hpp>
 #include <iostream>
+#include <optional>
 #include <parser/tokenize.hpp>
 
 void minsh::controller::ShellController::run() {
   running = true;
+  std::optional<double> prev_time = std::nullopt;
+
   while (running) {
+    if (char cwd[1024]; getcwd(cwd, sizeof(cwd)) != nullptr) {
+      std::cout << "\033[0;36m" << cwd << "\033[0m";
+      if (prev_time.has_value()) {
+        std::cout << " took \033[0;33m" << prev_time.value() << "\033[0ms";
+      }
+      std::cout << std::endl << "\033[0;31m$\033[0m " << std::flush;
+    } else {
+      std::cout << "\033[0;31mminsh>\033[0m ";
+      break;
+    }
+
     std::string input;
-    std::cout << "minsh> ";
-    std::getline(std::cin, input);
+
+    if (!std::getline(std::cin, input)) {
+      stop();
+      break;
+    }
 
     auto tokens = parser::tokenize(input);
-    int code = execute(tokens);
+    if (tokens.empty()) {
+      continue;
+    }
+
+    const auto start_time = std::chrono::high_resolution_clock::now();
+
+    if (int code = execute(tokens); code != EXIT_SUCCESS) {
+      std::cerr << "minsh: command failed with code " << code << std::endl;
+    }
+
+    const auto end_time = std::chrono::high_resolution_clock::now();
+
+    prev_time = std::chrono::duration<double>(end_time - start_time).count();
+
+    std::cout << std::endl;
   }
 }
 
