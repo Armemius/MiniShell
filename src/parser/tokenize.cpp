270d5fe09902e3c59611c5a1918c31956e07e0dd
Armemius <rusgy@yandex.ru>
2025-01-23 19:15:23 +0300

    [+] Improve tokenizer

--- src/parser/tokenize.cpp
+++ src/parser/tokenize.cpp
@@ -1,12 +1,19 @@
 #include <parser/tokenize.hpp>
-#include <sstream>
+#include <regex>
 
 std::vector<std::string> minsh::parser::tokenize(const std::string &input) {
   std::vector<std::string> tokens;
-  std::istringstream stream(input);
-  std::string token;
+  const std::regex token_regex(R"((\"[^\"]*\"|\'[^\']*\'|\S+))");
+  const auto words_begin =
+      std::sregex_iterator(input.begin(), input.end(), token_regex);
+  const auto words_end = std::sregex_iterator();
 
-  while (stream >> token) {
+  for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
+    std::string token = (*i).str();
+    if (token.front() == '\"' && token.back() == '\"' ||
+        token.front() == '\'' && token.back() == '\'') {
+      token = token.substr(1, token.size() - 2);
+    }
     tokens.push_back(token);
   }
 
