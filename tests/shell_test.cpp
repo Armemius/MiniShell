24df1492f284d0fc870b3ae4fd40f4168710973e
Armemius <rusgy@yandex.ru>
2025-01-23 20:47:20 +0300

    [+] Setup tests

--- tests/shell_test.cpp
+++ tests/shell_test.cpp
@@ -1,6 +1,125 @@
 #include <gtest/gtest.h>
 
-TEST(ShellTest, BasicAssertions) {
-  EXPECT_STRNE("hello", "world");
-  EXPECT_EQ(7 * 6, 42);
+#include <controller/controller.hpp>
+#include <controller/executors/external.hpp>
+#include <controller/executors/internal.hpp>
+
+TEST(ShellTest, TestEmpty) {
+  const auto controllerPtr =
+      std::make_shared<minsh::controller::ShellController>();
+
+  int code = controllerPtr->execute({});
+  EXPECT_EQ(code, EXIT_FAILURE);
+
+  code = controllerPtr->execute({""});
+  EXPECT_EQ(code, EXIT_FAILURE);
+}
+
+TEST(ShellTest, TestInternal) {
+  const auto controllerPtr =
+      std::make_shared<minsh::controller::ShellController>();
+
+  controllerPtr->addExecutor(
+      std::make_shared<minsh::executor::InternalExecutor>(controllerPtr));
+
+  int code = controllerPtr->execute({"cd"});
+  EXPECT_EQ(code, EXIT_FAILURE);
+
+  code = controllerPtr->execute({"cd", "nonexistent"});
+  EXPECT_EQ(code, EXIT_FAILURE);
+
+  code = controllerPtr->execute({"cd", "/tmp"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"cd", "/nonexistent"});
+  EXPECT_EQ(code, EXIT_FAILURE);
+
+  code = controllerPtr->execute({"cd", "/tmp"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"cd", "/tmp"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"cd", "/nonexistent"});
+  EXPECT_EQ(code, EXIT_FAILURE);
+
+  code = controllerPtr->execute({"cd", "/tmp"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"cd", "/tmp"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"cd", "/nonexistent"});
+  EXPECT_EQ(code, EXIT_FAILURE);
+
+  code = controllerPtr->execute({"cd", "/tmp"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"cd", "/tmp"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"cd", "/nonexistent"});
+  EXPECT_EQ(code, EXIT_FAILURE);
+
+  code = controllerPtr->execute({"cd", "/tmp"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"cd", "/tmp"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"cd", "/nonexistent"});
+  EXPECT_EQ(code, EXIT_FAILURE);
+
+  code = controllerPtr->execute({"cd", "/tmp"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"cd", "/tmp"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"cd", "/nonexistent"});
+  EXPECT_EQ(code, EXIT_FAILURE);
+
+  code = controllerPtr->execute({"cd", "/tmp"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"cd", "/tmp"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"cd", "/tmp", "/tmp"});
+  EXPECT_EQ(code, EXIT_FAILURE);
+
+  code = controllerPtr->execute({":"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({":", ":"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({":", "some sample text bla bla"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({":", "cd"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"pwd"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"pwd", "bla bla"});
+  EXPECT_EQ(code, EXIT_FAILURE);
+}
+
+TEST(ShellTest, TestExternal) {
+  const auto controllerPtr =
+      std::make_shared<minsh::controller::ShellController>();
+
+  controllerPtr->addExecutor(
+      std::make_shared<minsh::executor::ExternalExecutor>());
+
+  int code = controllerPtr->execute({"ls"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"ls", "-l"});
+  EXPECT_EQ(code, EXIT_SUCCESS);
+
+  code = controllerPtr->execute({"some nonexistent cmd", "-l", "-a"});
+  EXPECT_NE(code, EXIT_SUCCESS);
 }
