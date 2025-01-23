f256d35583e645df09b38c708f9ce58b5576e300
Armemius <rusgy@yandex.ru>
2025-01-23 19:40:37 +0300

    [+] Add proper time formatting

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -4,6 +4,40 @@
 #include <optional>
 #include <parser/tokenize.hpp>
 
+#include "fmt/compile.h"
+
+std::string formatTime(const double duration) {
+  constexpr double NS_THRESHOLD = 1e-6;
+  constexpr double US_THRESHOLD = 1e-3;
+  constexpr double MS_THRESHOLD = 1;
+  constexpr double M_THRESHOLD = 60;
+  constexpr double H_THRESHOLD = 60 * M_THRESHOLD;
+  constexpr double D_THRESHOLD = 24 * H_THRESHOLD;
+
+  if (duration < NS_THRESHOLD) {
+    return fmt::format("{}ns", static_cast<int>(duration * 1e9));
+  }
+  if (duration < US_THRESHOLD) {
+    return fmt::format("{}us", static_cast<int>(duration * 1e6));
+  }
+  if (duration < MS_THRESHOLD) {
+    return fmt::format("{}ms", static_cast<int>(duration * 1e3));
+  }
+  if (duration > D_THRESHOLD) {
+    return fmt::format("{}D {}", static_cast<int>(duration / D_THRESHOLD),
+                       formatTime(std::fmod(duration, D_THRESHOLD)));
+  }
+  if (duration > H_THRESHOLD) {
+    return fmt::format("{}H {}", static_cast<int>(duration / H_THRESHOLD),
+                       formatTime(std::fmod(duration, H_THRESHOLD)));
+  }
+  if (duration > M_THRESHOLD) {
+    return fmt::format("{}m {}", static_cast<int>(duration / M_THRESHOLD),
+                       formatTime(std::fmod(duration, M_THRESHOLD)));
+  }
+  return fmt::format("{}s", static_cast<int>(duration));
+}
+
 void minsh::controller::ShellController::run() {
   running = true;
   std::optional<double> prev_time = std::nullopt;
@@ -13,7 +47,8 @@ void minsh::controller::ShellController::run() {
     if (char cwd[1024]; getcwd(cwd, sizeof(cwd)) != nullptr) {
       std::cout << "\033[0;36m" << cwd << "\033[0m";
       if (prev_time.has_value()) {
-        std::cout << " took \033[0;33m" << prev_time.value() << "\033[0ms";
+        std::cout << " took \033[0;33m" << formatTime(prev_time.value())
+                  << "\033[0m";
       }
     }
     if (prev_code != EXIT_SUCCESS) {
